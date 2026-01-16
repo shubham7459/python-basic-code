bea938e85ca675ea91a11aeadc87ae1b5b62c7b8
Shubham yadav <sy196335@gmail.com>
2026-01-16 14:25:57 +0530

    bio

--- sum.py
+++ sum.py
@@ -1,4 +1,6 @@
-area = int(input("Enter the vaule of redius :"))
-pi = 3.14
-area_of_circle = pi * area * area
-print("The area of circle is :", area_of_circle)    
+country = {
+    "name" : "Shubam",
+    "age"  : 21,
+    "class" : "34"   
+}
+print(country)
