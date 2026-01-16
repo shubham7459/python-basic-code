c816325e89d8a6204c913083a9a472880ba1be4a
Shubham yadav <sy196335@gmail.com>
2026-01-16 14:32:41 +0530

    what

--- sum.py
+++ sum.py
@@ -1,6 +1,18 @@
-country = {
+bio = {
+
     "name" : "Shubam",
+
     "age"  : 21,
+
     "class" : "34"   
+
 }
-print(country)
+
+print(bio)
+print(bio["name"])
+bio["phone"] = 1231233
+print(bio)
+bio["name"] = "yadav"
+print(bio)
+del bio["phone"]
+print(bio)
